d25d74d17760fae36f2e519729ab28af6395ef12
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-06-29 21:28:20 +0530

    Print all possible subsets of a set, O(n * (2^n))

---
+++ Bit Manipulation/AllPossibleSubsets.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void possibleSubsets(int n, int *arr)
+{
+    for (int i = 0; i < (1 << n); i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i & (1 << j))
+            {
+                cout << arr[j] << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    possibleSubsets(n, arr);
+
+    delete[] arr;
+    arr = NULL;
+
+    return 0;
+}
