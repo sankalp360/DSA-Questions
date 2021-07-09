cf68de67f09595701ea97e3a3631e3a6f7199808
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-07-09 22:09:55 +0530

    number of ways to tile board using tiles, O(2^n)

---
+++ Recursion/TilingProblem.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int tilingWays(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    if (n == 0)
+    {
+        return 0;
+    }
+    return tilingWays(n - 1) + tilingWays(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << tilingWays(n) << endl;
+    return 0;
+}
