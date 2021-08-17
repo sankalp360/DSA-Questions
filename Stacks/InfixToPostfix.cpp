d58db34b9e28194cb063e9e63314adc4b0c316df
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-08-17 20:02:07 +0530

    Infix To Postfix expression Conversion using Stack

---
+++ Stacks/InfixToPostfix.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int precedence(char c)
+{
+    switch (c)
+    {
+    case '^':
+        return 3;
+        break;
+    case '/':
+        return 2;
+        break;
+    case '*':
+        return 2;
+        break;
+    case '+':
+        return 1;
+        break;
+    case '-':
+        return 1;
+        break;
+    default:
+        return -1;
+    }
+}
+
+string infixToPostfix(string s)
+{
+    stack<char> st;
+    string res;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            res += s[i];
+        }
+        else if (s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            while (!st.empty() && st.top() != '(')
+            {
+                res += st.top();
+                st.pop();
+            }
+            if (!st.empty())
+            {
+                st.pop();
+            }
+        }
+        else
+        {
+            while (!st.empty() && precedence(st.top()) > precedence(s[i]))
+            {
+                res += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while (!st.empty())
+    {
+        res += st.top();
+        st.pop();
+    }
+    return res;
+}
+
+int main()
+{
+    cout << infixToPostfix("(a-b/c)*(a/k-l)") << endl;
+    return 0;
+}
