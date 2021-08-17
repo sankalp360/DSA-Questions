9d8ed25e4412ddc05ddb124b59b8a64b6044253b
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-08-17 19:32:51 +0530

    Quick Sort, Recursive Implementation

---
+++ Algorithms/Sorting/QuickSort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std; //using standards
+
+int partition(int *arr, int l, int r)
+{
+    int pivot = arr[r];
+    int i = l - 1;
+
+    for (int j = l; j < r; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[r]);
+    return i + 1;
+}
+
+void quickSort(int *arr, int l, int r)
+{
+    if (l < r)
+    {
+        int pi = partition(arr, l, r);
+        quickSort(arr, l, pi - 1);
+        quickSort(arr, pi + 1, r);
+    }
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    // allocate the array
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    //deallocate the array
+    delete[] arr;
+    arr = NULL;
+
+    return 0;
+}
