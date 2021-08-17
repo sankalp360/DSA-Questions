cdafc2d1a632c32dc9a7e8c79b6f4615f1e8d8a3
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-08-17 19:32:28 +0530

    Merge Sort Recursive Implementation

---
+++ Algorithms/Sorting/MergeSort.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void merger(int *arr, int l, int mid, int r)
+{
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+
+    int *a = new int[n1];
+    int *b = new int[n2];
+
+    for (int i = 0; i < n1; i++)
+    {
+        a[i] = arr[l + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        b[i] = arr[mid + 1 + i];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+
+    while (i < n1 && j < n2)
+    {
+        if (a[i] < b[j])
+        {
+            arr[k] = a[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            arr[k] = b[j];
+            k++;
+            j++;
+        }
+    }
+
+    while (i < n1)
+    {
+        arr[k] = a[i];
+        k++;
+        i++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = b[j];
+        k++;
+        j++;
+    }
+
+    delete[] a;
+    delete[] b;
+    a = NULL;
+    b = NULL;
+}
+
+void mergeSort(int *arr, int l, int r)
+{
+    if (l < r)
+    {
+        int mid = (l + r) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+
+        merger(arr, l, mid, r);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr;
+    arr = NULL;
+    return 0;
+}
