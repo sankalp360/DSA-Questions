38252d498bc83a1d101eac85fd92cdf16a30d441
sanp9 <patel.san360@gmail.com>
2021-08-25 00:25:31 +0530

    .

--- Queue/queueUsingArray.cpp
+++ Queue/queueUsingArray.cpp
@@ -12,16 +12,47 @@ public:
     Queue(int size) : front(-1), rear(-1), size(size) {}
 
     void enqueue(int val);
+    int dequeue();
 };
 
 void Queue::enqueue(int val)
 {
+    if (rear < size - 1)
+    {
+        arr[++rear] = val;
+    }
+    else
+    {
+        cout << "Overflow" << endl;
+    }
+}
+
+int Queue::dequeue()
+{
+    int el = -1;
+    if (front != rear)
+    {
+        el = arr[front + 1];
+        front++;
+    }
+    else
+    {
+        cout << "Underflow" << endl;
+    }
+    return el;
 }
 
 int main()
 {
     int size;
-    cin >> size;
     Queue q(100);
     q.enqueue(2);
+    q.enqueue(3);
+    cout << q.dequeue() << " ";
+    cout << q.dequeue() << " ";
+    cout << q.dequeue() << " ";
+    for (int i = q.front + 1; i <= q.rear; i++)
+    {
+        cout << q.arr[i];
+    }
 }
